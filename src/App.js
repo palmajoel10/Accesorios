a5b4961fab7c81e01d70a02f48b8146be8f8090d
new name <new email>
2022-06-15 14:14:07 -0300

    a

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Productos from './components/Productos';
+
 
 class App extends Component {
+  state = {
+    Productos: [
+    { name: 'Reloj', price: 1500, img: '/Productos/reloj.jpg' },
+    { name: 'Visera', price: 2000, img: '/Productos/visera.jpg' },
+    { name: 'Lentes', price: 5000, img: '/Productos/lentes.jpg' },
+    ]
+  }
   render() {
     return (
       <div>
-        <p>hola mundo</p>
+        <Productos
+        agregarAlCarro={() => console.log('No hace nada')}
+        productos={this.state.productos}
+        />
       </div>
     )
   }
---
+++ src/components/Producto.js
@@ -0,0 +1,15 @@
+import Productos from './Productos'
+class Producto extends Component {
+    render() {
+        const { producto } = this.props
+        return (
+            <div>
+                <img alt={producto.name} src={producto.img} />
+                <h3>{producto.name}</h3>
+                <p>{producto.price}</p>
+            </div>
+        )
+    }
+}
+
+export default Producto
